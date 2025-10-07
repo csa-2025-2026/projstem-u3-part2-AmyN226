17ace3bd75b7b3a4c90f11da12837250d15f03f3
AmyN226 <anguyen226@student.hbuhsd.edu>
2025-10-07 21:45:19 +0000

    started

--- Main.java
+++ Main.java
@@ -7,6 +7,39 @@ public class Main
     Scanner scan = new Scanner(System.in);  // input from the keyboard
     /* ============= BEGIN SOLUTION ================================ */
 
+    System.out.println("Enter a number");
+    double target = 12.345;
+    double user_input = scan.nextDouble();
+
+    if (user_input == target)
+    {
+      System.out.println("YES!");
+    }
+
+    
+    System.out.println("Enter a number");
+    int target2 = 48;
+    int user_input2 = scan.nextInt();
+
+    if (user_input2 == target2)
+    {
+      System.out.println("YES!");
+    }
+
+    System.out.println("Enter a number");
+    int val1 = scan.nextInt();
+    System.out.println("Enter another number");
+    int val2 = scan.nextInt();
+
+    if ((2 * val1) == val2)
+    {
+      System.out.println("YES!");
+    }
+
+
+
+     System.out.println("Enter a number");
+    int val3 = scan.nextInt();
 
 
 
